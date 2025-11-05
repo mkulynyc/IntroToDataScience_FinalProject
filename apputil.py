6197b4b1ccd7b71ec2fa8648f51da63fdea9d03a
Matthew Kulynych <mkulynyc@iu.edu>
2025-11-05 17:11:22 -0500

    date changes pt 3

--- apputil.py
+++ apputil.py
@@ -318,7 +318,10 @@ def plot_actor_timeline(df, actor_name):
     df_actor = df[df['cast'].str.contains(actor_name, na=False)].copy()
     df_actor = df_actor.sort_values("release_year")
 
-    df_actor["start"] = pd.to_datetime(df_actor["release_year"], format="%Y", errors="coerce")
+    df_actor["start"] = pd.to_datetime(
+        df_actor["release_year"].apply(lambda x: str(int(float(x))) if pd.notna(x) else None),
+        format="%Y", errors="coerce"
+    )
     df_actor = df_actor.dropna(subset=["start"])
     df_actor["end"] = df_actor["start"] + pd.DateOffset(months=6)
 
