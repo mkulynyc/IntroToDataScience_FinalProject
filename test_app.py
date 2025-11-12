e601ad763cbb8063b1f22e0bcba2eea92cc82dd1
Matthew Kulynych <mkulynyc@iu.edu>
2025-11-12 17:25:06 -0500

    adding test app functions

---
+++ test_app.py
@@ -0,0 +1,13 @@
+### Imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import streamlit as st
+from tabulate import tabulate
+import plotly.express as px
+
+from data_load import *
+from plots import *
+from engine import *
