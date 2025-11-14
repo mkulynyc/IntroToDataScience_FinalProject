243585bb8d2d269698418fa2018c167a8dfa7f76
Matthew Kulynych <mkulynyc@iu.edu>
2025-11-13 20:27:00 -0500

    init files to folders for function imports

---
+++ src/apps/__init__.py
@@ -0,0 +1,4 @@
+from .enhanced_description_analyzer_combined import *
+from .enhanced_netflix_description_analyzer_app import *
+from .netflix_description_analyzer_app import *
+from .netflix_original_vs_licensed_classifier_app import *
---
+++ src/utils/__init__.py
@@ -0,0 +1,1 @@
+from .shared_utils import *
